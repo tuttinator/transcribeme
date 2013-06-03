2636e70cc6e585b435943182f462acb5d145523e
Caleb Tutty <caleb@prettymint.co.nz>
2013-06-03 17:06:05 +1200

    Added the first spec for the api gem

---
+++ spec/lib/api/client_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe TranscribeMe::API::Client do
+  
+  before :all do
+    @client = TranscribeMe::API::Client.new 
+  end
+
+  it "should be valid" do
+    @client.should_not be_nil
+  end
+
+end
