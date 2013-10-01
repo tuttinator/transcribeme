7d3a875f55975643f3be0deff615a4d3dee52cd5
Caleb Tutty <caleb@prettymint.co.nz>
2013-10-01 16:13:52 +1200

    Refactored specs to iterate through an array of symbols for respond_to declarations

--- spec/lib/recording_spec.rb
+++ spec/lib/recording_spec.rb
@@ -4,71 +4,57 @@ require 'spec_helper'
 
 describe TranscribeMe::Recording do
 
-  before :all do
-    ready_hash = {  date_created: DateTime.now, 
-                    duration: 200.59, 
-                    id: SecureRandom.uuid, 
-                    name: "Sample Recording", 
-                    status: 10, 
-                    state: "Ready for Transcription" }
-
-    @recording = TranscribeMe::Recording.new(ready_hash)
-  end
-
   describe 'Class Methods' do
 
     subject(:recordings) { TranscribeMe::Recording }
 
     describe '"respond_to"s' do
-    
-      it 'has an all method' do
-        expect(recordings).to respond_to :all
+
+      [:all, :list, :to_a, :new_from_soap, :inspect, :to_s, :completed, 
+        :in_progress, :processing_audio, :ready_for_transcription, :ready].each do |method|
+
+        it "has a #{method.to_s} method" do
+          expect(recordings).to respond_to method
+        end
+
       end
 
       it 'aliases the all method to list' do
-        expect(recordings).to respond_to :list
         expect(recordings.list).to eq recordings.all
       end
 
       it 'aliases the all method to to_a' do
-        expect(recordings).to respond_to :to_a
         expect(recordings.to_a).to eq recordings.all
       end
 
-      it 'has a new_from_soap method' do
-        expect(recordings).to respond_to :new_from_soap
-      end
-
-      it 'has an inspect method' do
-        expect(recordings).to respond_to :inspect
-      end
-
       it 'aliases the inspect method to to_s' do
-        expect(recordings).to respond_to :to_s
         expect(recordings.to_s).to eq recordings.inspect
       end
 
-      it 'has a completed method' do
-        expect(recordings).to respond_to :completed
-      end
-
-      it 'has a in_progress method' do
-        expect(recordings).to respond_to :in_progress
-      end
-
-      it 'has a processing_audio method' do
-        expect(recordings).to respond_to :processing_audio
-      end
-
-      it 'has a ready_for_transcription method' do
-        expect(recordings).to respond_to :ready_for_transcription
-      end
-
       it 'aliases the ready_for_transcription to ready' do
-        expect(recordings).to respond_to :ready
         expect(recordings.ready).to eq recordings.ready_for_transcription
       end
 
+    end
+  end
+
+  describe 'instance methods' do
+
+    before :all do
+      ready_hash = {  date_created: DateTime.now, 
+                      duration: 200.59, 
+                      id: SecureRandom.uuid, 
+                      name: "Sample Recording", 
+                      status: 10, 
+                      state: "Ready for Transcription" }
+
+      @recording = TranscribeMe::Recording.new(ready_hash)
+    end
+
+    [:date_created, :duration, :id, :name, :status, :state].each do |method|
+      it "has a #{method.to_s} method" do
+        expect(@recording).to respond_to method
+      end
     end
 
   end
