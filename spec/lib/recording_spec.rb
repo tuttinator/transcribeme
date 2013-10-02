7d0e9b6cbdc563cdeb9f705eb394891b2144f5ca
Caleb Tutty <caleb@prettymint.co.nz>
2013-10-02 21:45:08 +1300

    Added specs for states, etc

--- spec/lib/recording_spec.rb
+++ spec/lib/recording_spec.rb
@@ -51,10 +51,72 @@ describe TranscribeMe::Recording do
       @recording = TranscribeMe::Recording.new(ready_hash)
     end
 
-    [:date_created, :duration, :id, :name, :status, :state].each do |method|
+    [:date_created, :duration, :id, :name, :status, :state, :[], :keys].each do |method|
       it "has a #{method.to_s} method" do
         expect(@recording).to respond_to method
       end
+    end
+
+  end
+
+  describe 'private setter methods' do
+
+    before :each do
+      @recording = TranscribeMe::Recording.new({})
+    end
+
+    describe 'typecasting the duration to a float' do
+
+      it 'changes an integer to a float' do
+        @recording.duration = 32
+        expect(@recording.duration).to eq(32.0)
+        expect(@recording.duration.class).to eq(Float)
+      end
+
+      it 'changes a string to a float' do
+        @recording.duration = "32"
+        expect(@recording.duration).to eq(32.0)
+        expect(@recording.duration.class).to eq(Float)
+      end
+
+    end
+
+    describe 'the status attribute sets a human readable state string' do
+
+      describe 'typecasting the status to an integer' do
+
+        it 'changes a string to an integer' do
+          @recording.status = "10"
+          expect(@recording.status).to eq(10)
+          expect(@recording.status.class).to eq(Fixnum)
+        end
+
+      end
+
+      describe 'setting the state string' do
+
+        it 'sets the state string when the status code for ready' do
+          @recording.status = 10
+          expect(@recording.state).to eq('Ready for Transcription')
+        end
+
+        it 'sets the state string when the status code for Processing Audio' do
+          @recording.status = 35
+          expect(@recording.state).to eq('Processing Audio')
+        end
+
+        it 'sets the state string when the status code for In Progress' do
+          @recording.status = 40
+          expect(@recording.state).to eq('In Progress')
+        end
+
+        it 'sets the state string when the status code for Completed' do
+          @recording.status = 50
+          expect(@recording.state).to eq('Completed')
+        end
+
+      end
+
     end
 
   end
